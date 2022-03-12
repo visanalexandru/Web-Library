259012b0670413ab37b9f13c9ff4542fcd1236b5
visan <alexandruvisan44@yahoo.com>
2022-03-12 13:25:02 +0200

    Started to migrate to javascript.

---
+++ index.js
@@ -0,0 +1,30 @@
+express = require("express");
+app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/resurse", express.static(__dirname + "/resurse"))
+app.get(["/index", "/home"], function (req, res) {
+    res.render("pagini/index.ejs");
+}
+)
+
+app.get("/*", function (req, res) {
+    res.render("pagini" + req.url, function (err, rezRender) {
+        if (err) {
+            if (err.message.includes("Failed to lookup view")) {
+                res.status(404).render("pagini/404");
+            }
+            else {
+                res.render("pagini/eroare_generala");
+            }
+        }
+        else {
+            res.send(rezRender);
+        }
+    });
+
+    res.end();
+})
+
+app.listen(8080);
