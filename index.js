ddd778df6d7e6c8ace0deeafa32400372cc92cb1
visan <alexandruvisan44@yahoo.com>
2022-03-17 21:41:40 +0200

    Task 3.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-express = require("express");
+const express = require("express");
+const fs=require("fs");
+const sharp=require("sharp");
 app = express();
 
 app.set("view engine", "ejs");
 
 app.use("/resurse", express.static(__dirname + "/resurse"))
 app.get(["/","/index", "/home"], function (req, res) {
-    res.render("pagini/index.ejs",{ip:req.ip});
+    res.render("pagini/index.ejs",{ip:req.ip,imagini:obImagini.imagini});
 }
 )
 
@@ -31,4 +33,32 @@ app.get("/*", function (req, res) {
     res.end();
 })
 
+
+function creeazaImagini(){
+    var buf=fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf8");
+    obImagini=JSON.parse(buf);//global
+    //console.log(obImagini);
+    for (let imag of obImagini.imagini){
+        let nume_imag, extensie;
+        [nume_imag, extensie ]=imag.fisier.split(".")// "abc.de".split(".") ---> ["abc","de"]
+        let dim_mic=150
+        
+        imag.mic=`${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp` //nume-150.webp // "a10" b=10 "a"+b `a${b}`
+        imag.mare=`${obImagini.cale_galerie}/${imag.fisier}`;
+
+        if (!fs.existsSync(imag.mic)){
+            sharp(__dirname+"/"+imag.mare).resize(dim_mic).toFile(__dirname+"/"+imag.mic);
+        }
+
+        let dim_mediu=300
+        imag.mediu=`${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.png` 
+        if (!fs.existsSync(imag.mediu)){
+            sharp(__dirname+"/"+imag.mare).resize(dim_mediu).toFile(__dirname+"/"+imag.mediu);
+        }
+    }
+
+}
+
+
+creeazaImagini();
 app.listen(8080);
